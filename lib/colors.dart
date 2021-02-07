19516c689872e329fe92f35a80a0856e92c3f904
Ruvo99 <migruv5799@gmail.com>
2021-02-06 20:03:20 -0600

    Colors Class created

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const cuppingOrange = Color(0xFFEC9762);
+const cuppingYellow = Color(0xFFFABF7C);
+const cuppingGrey = Color(0xFFBCB0A1);
+const cuppingDarkGrey = Color(0xFF8B8175);
+const cuppingBlue = Color(0xFF214254);
+const cuppingBlack = Color(0xFF121B22);
+const cuppingWhite = Color(0xFFFFFFFF);
